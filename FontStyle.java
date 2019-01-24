ab62320d2b446cff76739aa19bd2761216549e0f
Saed Alewaity <saed_1996@hotmail.com>
2019-01-24 16:37:53 +0200

    Update FontStyle.java

--- FontStyle.java
+++ FontStyle.java
@@ -1,4 +1,3 @@
-package com.example.saedalewaity.product4ever.classes;
 
 import android.content.Context;
 import android.graphics.Typeface;
@@ -14,7 +13,7 @@ public class FontStyle<T> extends AppCompatActivity {
     *set  Font Type
     * one TextView
     **/
-    public void setFontType(Context context,String fontName,TextView txt1 ){
+    public static void setFontType(Context context, String fontName, TextView txt1){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),fontName+".ttf");
         txt1.setTypeface(tf);
     }
@@ -22,8 +21,8 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Two TextView
      **/
-    public void  setFontType(Context context ,String fontName, TextView txt1, TextView txt2){
-        Typeface tf = Typeface.createFromAsset(getAssets(),
+    public static void  setFontType(Context context, String fontName, TextView txt1, TextView txt2){
+        Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         txt1.setTypeface(tf);
         txt2.setTypeface(tf);
@@ -32,7 +31,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Three TextView
      **/
-    public void  setFontType(Context context ,String fontName, TextView txt1, TextView txt2, TextView txt3){
+    public static void  setFontType(Context context, String fontName, TextView txt1, TextView txt2, TextView txt3){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         txt1.setTypeface(tf);
@@ -43,7 +42,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Four TextView
      **/
-    public void  setFontType(Context context ,String fontName, TextView txt1, TextView txt2, TextView txt3, TextView txt4){
+    public static void  setFontType(Context context ,String fontName, TextView txt1, TextView txt2, TextView txt3, TextView txt4){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         txt1.setTypeface(tf);
@@ -55,7 +54,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Fife TextView
      **/
-    public void  setFontType(Context context ,String fontName, TextView txt1, TextView txt2, TextView txt3, TextView txt4, TextView txt5){
+    public static void  setFontType(Context context, String fontName, TextView txt1, TextView txt2, TextView txt3, TextView txt4, TextView txt5){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         txt1.setTypeface(tf);
@@ -68,7 +67,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * one EditText
      **/
-    public void  setFontType(Context context , String fontName, EditText edTxt1){
+    public static void  setFontType(Context context, String fontName, EditText edTxt1){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         edTxt1.setTypeface(tf);
@@ -77,7 +76,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Two EditText
      **/
-    public void  setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2){
+    public static void  setFontType(Context context, String fontName, EditText edTxt1, EditText edTxt2){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         edTxt1.setTypeface(tf);
@@ -87,7 +86,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Three EditText
      **/
-    public void  setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3){
+    public static void  setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         edTxt1.setTypeface(tf);
@@ -99,7 +98,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Four EditText
      **/
-    public void setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3, EditText edTxt4){
+    public static void setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3, EditText edTxt4){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         edTxt1.setTypeface(tf);
@@ -111,7 +110,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Fife EditText
      **/
-    public void  setFontType(Context context , String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3, EditText edTxt4, EditText edTxt5){
+    public static void  setFontType(Context context, String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3, EditText edTxt4, EditText edTxt5){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         edTxt1.setTypeface(tf);
@@ -122,9 +121,24 @@ public class FontStyle<T> extends AppCompatActivity {
     }
     /**
      *set  Font Type
+     * Six EditText
+     **/
+    public static void  setFontType(Context context, String fontName, EditText edTxt1, EditText edTxt2, EditText edTxt3, EditText edTxt4, EditText edTxt5, EditText edTxt6){
+        Typeface tf = Typeface.createFromAsset(context.getAssets(),
+                fontName+".ttf");
+        edTxt1.setTypeface(tf);
+        edTxt2.setTypeface(tf);
+        edTxt3.setTypeface(tf);
+        edTxt4.setTypeface(tf);
+        edTxt5.setTypeface(tf);
+        edTxt6.setTypeface(tf);
+
+    }
+    /**
+     *set  Font Type
      * Fife Button
      **/
-    public void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3,Button btn4,Button btn5){
+    public static void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3,Button btn4,Button btn5){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         btn1.setTypeface(tf);
@@ -137,7 +151,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Four Button
      **/
-    public void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3,Button btn4){
+    public static void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3,Button btn4){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         btn1.setTypeface(tf);
@@ -149,7 +163,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Three Button
      **/
-    public void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3){
+    public static void  setFontType(Context context , String fontName, Button btn1,Button btn2,Button btn3){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         btn1.setTypeface(tf);
@@ -160,7 +174,7 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * Two Button
      **/
-    public void  setFontType(Context context , String fontName, Button btn1,Button btn2){
+    public static void  setFontType(Context context, String fontName, Button btn1, Button btn2){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         btn1.setTypeface(tf);
@@ -171,10 +185,11 @@ public class FontStyle<T> extends AppCompatActivity {
      *set  Font Type
      * one Button
      **/
-    public void  setFontType(Context context , String fontName, Button btn1){
+    public static void  setFontType(Context context, String fontName, Button btn1){
         Typeface tf = Typeface.createFromAsset(context.getAssets(),
                 fontName+".ttf");
         btn1.setTypeface(tf);
     }
 
 }
+
